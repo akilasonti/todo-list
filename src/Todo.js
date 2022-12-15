5341c441d90d2f29085072f4c82e56b0ee480e0a
akilasonti <akilasonti9@gmail.com>
2022-12-15 20:05:32 +0530

    update functionality completed

--- src/Todo.js
+++ src/Todo.js
@@ -2,29 +2,43 @@ import "./Todo.css";
 import { useState } from "react";
 export function Todo() {
   const [todoList, setTodoList] = useState([]);
+  const [todoListFlag, setTodoListFlag] = useState([]);
   const [inp, setInp] = useState("");
   const [editField, updateEditField] = useState("");
-  const [flag, setFlag] = useState(false);
-  const edit = function () {};
+  // const [flag, setFlag] = useState(false);
+  // const edit = function () {};
+  function toggle(index){
+    const arr = todoListFlag;
+    arr.splice(index, 1, !arr[index]);
+    setTodoListFlag([...arr]);
+  }
   const update = function (index) {
     const arr = todoList;
     arr.splice(index, 1, editField);
     setTodoList([...arr]);
     updateEditField("");
-    setFlag(false);
+    toggle(index);
+    // setFlag(false);
   };
   const add = (event) => {
     //prevent to refresh the page on form submit
     event.preventDefault();
 
     const arr = todoList;
+    if(arr.includes(inp)){
+      alert('Entered value already exists! Please enter a new value!');
+      setInp("");
+      return false;
+    }
     arr.push(inp);
+    todoListFlag.push(false);
     setTodoList([...arr]);
     setInp("");
   };
   const deleteList = (idx) => {
     const arr = todoList;
     arr.splice(idx, 1);
+    todoListFlag.splice(idx, 1);
     setTodoList([...arr]);
   };
   return (
@@ -50,7 +64,7 @@ export function Todo() {
         {todoList.map((value, index) => {
           return (
             <div className="task-input2">
-              {flag ? (
+              {todoListFlag[index] ? (
                 <input
                   type="text"
                   value={editField}
@@ -61,7 +75,8 @@ export function Todo() {
               ) : (
                 <input type="text" value={value} disabled></input>
               )}
-              {flag ? (
+              {todoListFlag[index] ? (
+                <>
                 <button
                   className="updatebtn"
                   onClick={() => {
@@ -70,9 +85,16 @@ export function Todo() {
                 >
                   Update
                 </button>
+                <button onClick={()=>{
+                  toggle(index);updateEditField("");
+                }}>Cancel</button>
+                </>
               ) : (
                 <>
-                  <button className="fstbtn" onClick={() => setFlag(true)}>
+                  <button className="fstbtn" onClick={() =>{
+                    toggle(index);
+                    updateEditField(todoList[index]);
+                  }}>
                     Edit
                   </button>
                   <button
